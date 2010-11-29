9313c661ae50620e33aebabac69b50d371bf7d0b
Seth Vargo <svargo@cmu.edu>
2010-11-28 21:02:46 -0500

    added "how to get up and running" instructions to README, added www/.htaccess.example with comments, added default user to sql statement

--- db/quickeval.sql
+++ db/quickeval.sql
@@ -189,3 +189,8 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `active` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=876 ;
+
+--
+-- Insert a default User
+--
+INSERT INTO Users (id, university_id, fname, lname, `level`, email, invite_code, `password`, `timestamp`, last_login_ip, active) VALUES(876, 1, 'Super', 'Administrator', 4, 'test', 'too_cool_for_one', '04574dc93174cf8c4fd2e3c35cb4301b7a468feda7aa47d2c65c8eddbe22f6d8', '2010-11-28 20:11:45', '0.0.0.0', 1);
