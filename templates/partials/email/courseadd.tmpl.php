47a604aa65ba455b26844daa12cc315c6489ac8e
noahlevin <noah@speakertext.com>
2010-11-28 21:35:37 -0500

    quickeval.org NOT .me

--- templates/partials/email/courseadd.tmpl.php
+++ templates/partials/email/courseadd.tmpl.php
@@ -1,4 +1,4 @@
 <img src='<?php echo WEB_ROOT; ?>/images/email/graph.jpg' align='right' alt='Course Added' />
 
-Your professor, <?php echo $invitinguser->name(); ?> has added you to the course, <?php echo $course->name; ?>, in QuickEval.me.
+Your professor, <?php echo $invitinguser->name(); ?> has added you to the course, <?php echo $course->name; ?>, in quickeval.org.
 <br /><br />
